a9be846ae3ad7fbf1406d396ba5c1c8419f25164
Luffy <492580582@qq.com>
2018-06-17 00:55:58 +0800

    改变了写博客的页面，新增标签功能，服务器标签功能尚未完成

---
+++ templates/write_blog.js
@@ -0,0 +1,6 @@
+function choose_tag(event) {
+	// body...
+	var options_tag = ["散文","记叙文","议论文","诗歌","小说","戏剧","青春","社会","戏剧","现实","悲惨","休闲","人生","随笔"];
+	var options_status = options_tag.length*[0]
+
+}
